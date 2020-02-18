a7931e314ab558ad2b17c25f9c7a99b111587b5c
Dion Garman <diongarman@gmail.com>
2020-02-18 23:43:55 +0000

    CV work history timeline

--- src/app/components/cv/work-exp-item/work-exp-item.component.ts
+++ src/app/components/cv/work-exp-item/work-exp-item.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Input } from "@angular/core";
 
 @Component({
-  selector: 'app-work-exp-item',
-  templateUrl: './work-exp-item.component.html',
-  styleUrls: ['./work-exp-item.component.css']
+  selector: "app-work-exp-item",
+  templateUrl: "./work-exp-item.component.html",
+  styleUrls: ["./work-exp-item.component.css"]
 })
 export class WorkExpItemComponent implements OnInit {
+  @Input() workExpItem;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/components/cv/work-experience/work-experience.component.ts
+++ src/app/components/cv/work-experience/work-experience.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { workExperience } from "../../../data/work-exp-data";
 
 @Component({
-  selector: 'app-work-experience',
-  templateUrl: './work-experience.component.html',
-  styleUrls: ['./work-experience.component.css']
+  selector: "app-work-experience",
+  templateUrl: "./work-experience.component.html",
+  styleUrls: ["./work-experience.component.css"]
 })
 export class WorkExperienceComponent implements OnInit {
-
-  constructor() { }
+  workExperience;
+  constructor() {}
 
   ngOnInit(): void {
+    this.workExperience = workExperience;
   }
-
 }

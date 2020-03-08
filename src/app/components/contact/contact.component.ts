25ae66b34975bd9d2b4000691e35588f6d336703
Dion Garman <diongarman@gmail.com>
2020-03-08 16:31:03 +0000

    basic form validation set up

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder } from "@angular/forms";
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators
+} from "@angular/forms";
 
 @Component({
   selector: "app-contact",
@@ -7,21 +12,47 @@ import { FormBuilder } from "@angular/forms";
   styleUrls: ["./contact.component.css"]
 })
 export class ContactComponent implements OnInit {
-  contactForm;
-  constructor(private formBuilder: FormBuilder) {
+  contactForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
     this.contactForm = this.formBuilder.group({
-      emailAddress: "",
-      body: ""
+      emailAddress: new FormControl("", [
+        Validators.required,
+        Validators.email
+      ]),
+
+      body: new FormControl("", [Validators.required])
     });
   }
 
-  ngOnInit(): void {}
+  get fval() {
+    return this.contactForm.controls;
+  }
 
   onSubmit(customerData) {
     // Process checkout data here
 
-    this.contactForm.reset();
+    if (this.contactForm.invalid) {
+      alert("Form is invalid");
+      return;
+    }
 
+    this.contactForm.reset();
     console.warn("Your order has been submitted", customerData);
   }
+
+  get email() {
+    return this.contactForm.get("emailAddress");
+  }
 }
+
+/*
+
+
+RESOURCES
+https://jsonworld.com/demo/angular-8-reative-form-validation-example-and-tutorial
+https://angular.io/guide/form-validation#reactive-form-validation
+https://angular.io/start/start-forms
+
+*/
